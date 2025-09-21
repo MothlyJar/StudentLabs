0875f4be4efdee0ba428c8b94aea120dede99cad
MothlyJar <uliana.nikolaeva.s@mail.ru>
2025-09-21 20:40:00 +0200

    Create main.cpp

---
+++ L0_tests/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+// коммент
+/*
+длинный
+коммент
+*/
+
+int main() 
+{
+
+    std::cout << "Hello, world!" << std::endl; // standart::console out << << standart end
+    std::cout << "111" << std::endl;
+
+    return 0;
+}

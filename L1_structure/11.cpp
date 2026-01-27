61bb8e31fc2a29c5c4168cab6528612f89902ae4
MothlyJar <uliana.nikolaeva.s@mail.ru>
2026-01-27 20:54:35 +0200

    Upd L1

--- L1_structure/11.cpp
+++ L1_structure/11.cpp
@@ -3,13 +3,13 @@
 #include <string>
 using namespace std;
 
-struct Option { //выбор базового ингредиента зелья
+struct Option { 
     string description;
     int type;
 };
 
-void option_print(Option opts[], int n) { //распечатать опции, как в свитче по виду
-    for (int i = 0; i < n;i++) {
+void option_print(Option opts[], int n, int start) { 
+    for (int i = start; i < n;i++) {
         cout << opts[i].type << ": " << opts[i].description << '\n';
     }
 }
@@ -17,46 +17,49 @@ void option_print(Option opts[], int n) { //распечатать опции, �
 
 class Potion {
 private:
+    const float relative_zero = 0.001;
+    const int amount_of_bases = 8;
+
     string label_;
     string description_;
-    string base_;
     float volume_;
     int price_;
     int type_; 
 
-    Option bases[7]{
-        {"Water", 0},
-        {"Orange juice", 1},
-        {"Beer",2},
-        {"Unicorn's blood",3},
-        {"Abyss",4},
-        {"Head of Cerberus",5},
-        {"Set of unknown mushrooms",6}
+    Option bases[8]{
+        {"Air", 0},
+        {"Water", 1},
+        {"Orange juice", 2},
+        {"Beer",3},
+        {"Unicorn's blood",4},
+        {"Abyss",5},
+        {"Head of Cerberus",6},
+        {"Set of unknown mushrooms",7}
     };
+;
 public:
-    Potion() {
+    Potion() { //базовый
         label_ = "Untitled";
         description_ = "Empty bottle.";
-        base_ = "Nothing but an air.";
-        volume_ = 0;
+        volume_ = relative_zero;
         price_ = 0.1;
-        type_ = -1;
-        cout << "You took an empty bottle.\n";
+        type_ = 0;
+        cout << "[construct] You took an empty bottle.\n";
     }
     Potion(const Potion& other) { // копирование
         label_ = other.label_;
         description_ = other.description_;
-        base_ = other.base_;
         volume_ = other.volume_;
         price_ = other.price_;
         type_ = other.type_;
+        cout << "[construct] You brewed exactly the same potion as before.\n";
     }
     Potion(string label, string descriprion, int type_of_base, float volume, int price) : //полного заполнения
-        label_(label), description_(descriprion), base_(bases[type_of_base].description), volume_(volume), price_(price) { 
-        cout << "You brewed new potion.\n";
+        label_(label), description_(descriprion), volume_(volume), price_(price) { 
+        cout << "[construct] You brewed new potion.\n";
     }
     ~Potion() {
-        cout << this << "destruct";
+        cout << this << "[destruct] Your potion turned sour.";
     }
 
     void set_price(int price) {
@@ -65,37 +68,54 @@ public:
         }
         price_ = price;
     }
-
     void change_base() {
         cout << "Please, choose the basic ingredient:\n";
-        option_print(bases,7);
-        cin >> type_;
-        base_ = bases[type_].description; // по индексу(типу) выводим, что за база
-        volume_ += 0.5;
-    }
-    //void change_base(string base) { // базовая смена базы через приём аргумента в функции
-    //    cout << "You have changed the base.";
-    //    base_ = base;
-    //    volume_ += 0.5;
-    //}
-
-    void drink() {
-        if (volume_ > 0) {
-            volume_ -= 0.1;
-            cout << "You open a bottle and make a tiny sip.";
+        option_print(bases,amount_of_bases,1);
+        int t;
+        cin >> t;
+        if (!(1<=t<=amount_of_bases)) {
+            throw invalid_argument("There're no such a base in our basement.");
         }
         else {
-            cout << "The glass is already empty.";
+            type_ = t;
+            volume_ += 0.3;
+            cout << "You added " << bases[type_].description << " into your cattle. Now you have different potion.\n";
+        }
+    }
+    void empty_the_glass() {
+        if (volume_ <= relative_zero) {
+            cout << "The glass is already empty.\n";
+        }
+        else {
+            volume_ = relative_zero;
+            type_ = 0;
+            cout << "You poured the potion out, only little drops remain.\n";
+        }
+    }
+    void drink() {
+        if (volume_ <= relative_zero) {
+            cout << "The glass is already empty.\n";
+        }
+        else {
+            volume_ -= 0.1;
+            cout << "You open a bottle and make a tiny sip.\n";
         }  
     }
 
-    string get_label() const { return label_; } // manual value return 
-    string get_description() const { return description_; }
-    float get_volume() const { return volume_; }
-    int get_price() const { return price_; }
+    const string get_label() const { return label_; } // manual value return 
+    const string get_description() const { return description_; }
+    const float get_volume() const { return volume_; }
+    const int get_price() const { return price_; }
 
-    void print_base() const { cout << "Brewed with " + base_ + " as basic ingredient.\n"; } // with cout
-    void print_info() const {
+    const void print_base() const { // cout included
+        if (type_ == 0) {
+            cout << "Nothing but an air.\n";
+        }
+        else {
+            cout << "Brewed with " + bases[type_].description + " as basic ingredient.\n"; 
+        }
+    }
+    const void print_info() const {
         cout << get_label() << '\n' << get_description() << '\n';
         print_base();
         cout << "V: " << get_volume() << "\nC: " << get_price() << '\n';
@@ -106,7 +126,9 @@ int main() {
 
     try {
         Potion N;
-
+        N.change_base();
+        N.print_info();
+;
     }
     catch (exception ex) {
         cout << ex.what();

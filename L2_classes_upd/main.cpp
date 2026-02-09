e9ae76e57cbfb52a0bdb76a31febc6cf35b4cf5b
MothlyJar <uliana.nikolaeva.s@mail.ru>
2026-02-09 20:17:48 +0200

    now it's working at least

--- L2_classes_upd/main.cpp
+++ L2_classes_upd/main.cpp
@@ -5,11 +5,11 @@ using namespace std;
 int main() {
 
     try {
-        Potion N("", "aaa", { "b","c" }, 6, 0.6, 6);
-        //Potion D("b", "bbb", { "v","w" }, 2, 0.3, 4);
+        Potion N("a", "aaa", { "b","c" }, 6, 0.6, 6);
+        Potion D("b", "bbb", { "v","w" }, 2, 0.3, 4);
         //(N + D).print_info();
         //(N += D).print_info();
-        //(N*D).print_info();
+        (N*D).print_info();
     }
     catch (exception ex) {
         cout << ex.what();

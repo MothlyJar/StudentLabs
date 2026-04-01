50f5d7d445c54e78cbde2b98719889a03a81c6ec
MothlyJar <uliana.nikolaeva.s@mail.ru>
2026-04-01 12:57:20 +0200

    Update mainwindow.cpp

--- L5/mainwindow.cpp
+++ L5/mainwindow.cpp
@@ -8,6 +8,7 @@
 QString path_to_file;
 std::vector<Characters*> characters;
 
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
@@ -20,59 +21,68 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
 void MainWindow::on_Load_pB_clicked()
 {
+    ui->exceptions->clear();
+    path_to_file = QFileDialog::getOpenFileName(this, "Выберите .txt файл","/home","*.txt");
     if (path_to_file.isEmpty()) {
         ui->exceptions->setText("Нет ссылки на файл загрузки.");
     }
     else {
+        std::vector<std::string> lines;
         std::ifstream file(path_to_file.toStdString());
         while (!file.eof()) {
-            ui->exceptions->clear();
-            int identify; std::string name;
-            file >> identify >> name;
-            /*bool repeat_f = 0;
-            for(Characters* character : characters) {
-                if(character->get_name() == name) {
-                    repeat_f = 1;
-                    break;
-                }
+            std::string s;
+            getline(file,s);
+            lines.emplace_back(s);
+        }
+        int amount_of_lines = lines.size();
+        for (int line_i = 0; line_i < amount_of_lines - 1; line_i++) {
+            char identify; std::string name; std::string additional_str; int additional_int; int health; Armor armor;
+            std::string current_line = lines[line_i];
+            identify = current_line[0];
+            current_line.erase(0,2);
+
+            name = current_line.substr(0,current_line.find_first_of(' '));
+            current_line.erase(0,current_line.find_first_of(' ')+1);
+            additional_str = current_line.substr(0,current_line.find_first_of(' '));
+            current_line.erase(0,current_line.find_first_of(' ')+1);
+            additional_int = std::stoi(current_line.substr(0,current_line.find_first_of(' ')));
+            current_line.erase(0,current_line.find_first_of(' ')+1);
+            health = std::stoi(current_line.substr(0,current_line.find_first_of(' ')));
+            current_line.erase(0,current_line.find_first_of(' ')+1);
+            armor.helmet = std::stoi(current_line.substr(0,current_line.find_first_of(' ')));
+            current_line.erase(0,current_line.find_first_of(' ')+1);
+            armor.chestplate = std::stoi(current_line.substr(0,current_line.find_first_of(' ')));
+            current_line.erase(0,current_line.find_first_of(' ')+1);
+            armor.boots = std::stoi(current_line.substr(0,current_line.find_first_of(' ')));
+
+            if (identify == '1') {
+                Characters* Current_character = new MageChara(name,health,armor,additional_str,additional_int);
+                characters.emplace_back(Current_character);
             }
-            if (repeat_f == 1){
-                ui->exceptions->setText("Данные этого (этих) персонажа(жей) уже были выгружены.");
+            else if (identify == '0') {
+                Characters* Current_character = new EnemyChara (name,health,armor,additional_str,additional_int);
+                characters.emplace_back(Current_character);
+            }
+            else {
+                ui->exceptions->setText("Неопознанный идентификатор.");
                 continue;
             }
-            else {*/
-                std::string additional_str; int additional_int; int health; Armor armor;
-                file >> additional_str >> additional_int >> health >> armor.helmet >> armor.chestplate >> armor.boots;
-                if (identify == 0) {
-                    Characters* Current_character = new MageChara(name,health,armor,additional_str,additional_int);
-                    characters.emplace_back(Current_character);
-                }
-                else if (identify == 1) {
-                    Characters* Current_character = new EnemyChara (name,health,armor,additional_str,additional_int);
-                    characters.emplace_back(Current_character);
-                }
-                else {
-                    ui->exceptions->setText("Неопознанный идентификатор.");
-                    continue;
-                }
 
 
-                QString item_data_t = QString("%1 %2 %3 %4 <%5/%6/%7>").arg(name,additional_str).arg(additional_int).arg(health).arg(armor.helmet).arg(armor.chestplate).arg(armor.boots);
-                QPushButton* item_data_pB = new(QPushButton);
-                item_data_pB->setText(item_data_t);
-                ui->verticalLayout->addWidget(item_data_pB);
-            //}
+            QStringList item_data_lt = (QStringList() << QString("%1").arg(name) << QString("%1").arg(additional_str) << QString("%1").arg(additional_int) << QString("%1").arg(health) << QString("<%1/%2/%3>").arg(armor.helmet, armor.chestplate,armor.boots));
+            QTreeWidgetItem item_data_tree(item_data_lt);
+            QTreeWidgetItem* item_data_tree_p = new QTreeWidgetItem;
+            *item_data_tree_p = item_data_tree;
+            ui->treeWidget->addTopLevelItem(item_data_tree_p);
+
 
         }
         file.close();
     }
+
 }
 
-
-void MainWindow::on_ChooseFile_pB_clicked()
-{
-        path_to_file = QFileDialog::getOpenFileName(this, "Выберите файл","/home","*.txt");
-}
 

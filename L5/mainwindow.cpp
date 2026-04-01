1d86126a03d314a62a8153f5a393178a8c19567a
MothlyJar <uliana.nikolaeva.s@mail.ru>
2026-04-01 12:57:50 +0200

    Update mainwindow.cpp

--- L5/mainwindow.cpp
+++ L5/mainwindow.cpp
@@ -1,88 +1,113 @@
-#include "mainwindow.h"
-#include "ui_mainwindow.h"
-#include <QFileDialog>
-#include <fstream>
-#include <vector>
-#include "characters.h"
-
-QString path_to_file;
-std::vector<Characters*> characters;
-
-
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
-{
-    ui->setupUi(this);
-}
-
-MainWindow::~MainWindow()
-{
-    delete ui;
-}
-
-
-void MainWindow::on_Load_pB_clicked()
-{
-    ui->exceptions->clear();
-    path_to_file = QFileDialog::getOpenFileName(this, "Выберите .txt файл","/home","*.txt");
-    if (path_to_file.isEmpty()) {
-        ui->exceptions->setText("Нет ссылки на файл загрузки.");
-    }
-    else {
-        std::vector<std::string> lines;
-        std::ifstream file(path_to_file.toStdString());
-        while (!file.eof()) {
-            std::string s;
-            getline(file,s);
-            lines.emplace_back(s);
-        }
-        int amount_of_lines = lines.size();
-        for (int line_i = 0; line_i < amount_of_lines - 1; line_i++) {
-            char identify; std::string name; std::string additional_str; int additional_int; int health; Armor armor;
-            std::string current_line = lines[line_i];
-            identify = current_line[0];
-            current_line.erase(0,2);
-
-            name = current_line.substr(0,current_line.find_first_of(' '));
-            current_line.erase(0,current_line.find_first_of(' ')+1);
-            additional_str = current_line.substr(0,current_line.find_first_of(' '));
-            current_line.erase(0,current_line.find_first_of(' ')+1);
-            additional_int = std::stoi(current_line.substr(0,current_line.find_first_of(' ')));
-            current_line.erase(0,current_line.find_first_of(' ')+1);
-            health = std::stoi(current_line.substr(0,current_line.find_first_of(' ')));
-            current_line.erase(0,current_line.find_first_of(' ')+1);
-            armor.helmet = std::stoi(current_line.substr(0,current_line.find_first_of(' ')));
-            current_line.erase(0,current_line.find_first_of(' ')+1);
-            armor.chestplate = std::stoi(current_line.substr(0,current_line.find_first_of(' ')));
-            current_line.erase(0,current_line.find_first_of(' ')+1);
-            armor.boots = std::stoi(current_line.substr(0,current_line.find_first_of(' ')));
-
-            if (identify == '1') {
-                Characters* Current_character = new MageChara(name,health,armor,additional_str,additional_int);
-                characters.emplace_back(Current_character);
-            }
-            else if (identify == '0') {
-                Characters* Current_character = new EnemyChara (name,health,armor,additional_str,additional_int);
-                characters.emplace_back(Current_character);
-            }
-            else {
-                ui->exceptions->setText("Неопознанный идентификатор.");
-                continue;
-            }
-
-
-            QStringList item_data_lt = (QStringList() << QString("%1").arg(name) << QString("%1").arg(additional_str) << QString("%1").arg(additional_int) << QString("%1").arg(health) << QString("<%1/%2/%3>").arg(armor.helmet, armor.chestplate,armor.boots));
-            QTreeWidgetItem item_data_tree(item_data_lt);
-            QTreeWidgetItem* item_data_tree_p = new QTreeWidgetItem;
-            *item_data_tree_p = item_data_tree;
-            ui->treeWidget->addTopLevelItem(item_data_tree_p);
-
-
-        }
-        file.close();
-    }
-
-}
-
-
+<?xml version="1.0" encoding="UTF-8"?>
+<ui version="4.0">
+ <class>MainWindow</class>
+ <widget class="QMainWindow" name="MainWindow">
+  <property name="geometry">
+   <rect>
+    <x>0</x>
+    <y>0</y>
+    <width>782</width>
+    <height>621</height>
+   </rect>
+  </property>
+  <property name="windowTitle">
+   <string>MainWindow</string>
+  </property>
+  <widget class="QWidget" name="centralwidget">
+   <widget class="QPushButton" name="Load_pB">
+    <property name="geometry">
+     <rect>
+      <x>290</x>
+      <y>500</y>
+      <width>181</width>
+      <height>29</height>
+     </rect>
+    </property>
+    <property name="styleSheet">
+     <string notr="true">background: rgb(12, 190, 255);</string>
+    </property>
+    <property name="text">
+     <string>Загрузить</string>
+    </property>
+   </widget>
+   <widget class="QFrame" name="OuterFrame">
+    <property name="geometry">
+     <rect>
+      <x>60</x>
+      <y>40</y>
+      <width>661</width>
+      <height>441</height>
+     </rect>
+    </property>
+    <property name="frameShape">
+     <enum>QFrame::Shape::StyledPanel</enum>
+    </property>
+    <property name="frameShadow">
+     <enum>QFrame::Shadow::Raised</enum>
+    </property>
+    <widget class="QTreeWidget" name="treeWidget">
+     <property name="geometry">
+      <rect>
+       <x>30</x>
+       <y>20</y>
+       <width>601</width>
+       <height>391</height>
+      </rect>
+     </property>
+     <property name="font">
+      <font>
+       <pointsize>16</pointsize>
+      </font>
+     </property>
+     <property name="columnCount">
+      <number>5</number>
+     </property>
+     <attribute name="headerVisible">
+      <bool>false</bool>
+     </attribute>
+     <column>
+      <property name="text">
+       <string notr="true">1</string>
+      </property>
+     </column>
+     <column>
+      <property name="text">
+       <string notr="true">2</string>
+      </property>
+     </column>
+     <column>
+      <property name="text">
+       <string notr="true">3</string>
+      </property>
+     </column>
+     <column>
+      <property name="text">
+       <string notr="true">4</string>
+      </property>
+     </column>
+     <column>
+      <property name="text">
+       <string notr="true">5</string>
+      </property>
+     </column>
+    </widget>
+   </widget>
+   <widget class="QLabel" name="exceptions">
+    <property name="geometry">
+     <rect>
+      <x>60</x>
+      <y>10</y>
+      <width>371</width>
+      <height>20</height>
+     </rect>
+    </property>
+    <property name="text">
+     <string/>
+    </property>
+   </widget>
+  </widget>
+  <widget class="QStatusBar" name="statusbar"/>
+ </widget>
+ <resources/>
+ <connections/>
+</ui>
